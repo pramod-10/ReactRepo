0c1eff67301f7be04bd251b9511d0611fdfed217
pramod <83.pramod@gmail.com>
2019-12-01 10:42:58 +0530

    Updates

---
+++ precache-manifest.4da493a679d8323e3e4d143761028ae0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "112c7de0d070532fa8c56e5a121282f7",
+    "url": "/ReactRepo/index.html"
+  },
+  {
+    "revision": "a0a6e2063bebb921acc0",
+    "url": "/ReactRepo/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "ddd373ea9961ac24b9a2",
+    "url": "/ReactRepo/static/js/2.c766dd71.chunk.js"
+  },
+  {
+    "revision": "a0a6e2063bebb921acc0",
+    "url": "/ReactRepo/static/js/main.1d4ad0fa.chunk.js"
+  },
+  {
+    "revision": "8164bd162a72cfeb2a1c",
+    "url": "/ReactRepo/static/js/runtime-main.cf8160fc.js"
+  }
+]);
